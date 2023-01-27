045a3c45358caf50d8227436cb15b7edfb59dbeb
Bastian <bastian.bastias@gmail.com>
2023-01-27 17:52:55 -0500

    clone

--- src/ca/centennial/comp254/exercise1/bastian/bastias/Exercise3.java
+++ src/ca/centennial/comp254/exercise1/bastian/bastias/Exercise3.java
@@ -1,5 +1,8 @@
 package ca.centennial.comp254.exercise1.bastian.bastias;
 
+import linkedlists.CircularlyLinkedList;
+import linkedlists.CircularlyLinkedList.Node;
+
 public class Exercise3 {
 
 	public static void main(String[] args) {
@@ -23,5 +26,25 @@ public class Exercise3 {
 		}
 		
 	}
+	
+//	public CircularlyLinkedList<E> cloneOnClass() throws CloneNotSupportedException {
+//		// always use inherited Object.clone() to create the initial copy
+//		CircularlyLinkedList<E> other = (CircularlyLinkedList<E>) super.clone(); // safe cast
+//		if (size > 0) { // we need independent chain of nodes
+////			other.tail.setNext(new Node<>(tail.getElement(), null));
+//			other.tail.setNext(new Node<>(tail.getNext().getElement(), null));
+//			
+//			Node<E> walk = tail.getNext().getNext(); // walk through remainder of original list
+//			Node<E> otherTail = other.tail.getNext(); // remember most recently created node
+//			while (walk != null) { // make a new node storing same element
+//				Node<E> newest = new Node<>(walk.getElement(), null);
+//				otherTail.setNext(newest); // link previous node to this one
+//				otherTail = newest;
+//				walk = walk.getNext();
+//			}
+//			otherTail=tail;
+//		}
+//		return other;
+//	}
 
 }
