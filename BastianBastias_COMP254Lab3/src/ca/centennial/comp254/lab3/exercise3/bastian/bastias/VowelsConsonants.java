3fc521a4d50006d97f979e7f496f62b606687591
Bastian <bastian.bastias@gmail.com>
2023-02-16 10:33:12 -0500

    added exeercise 3

--- BastianBastias_COMP254Lab3/src/ca/centennial/comp254/lab3/exercise3/bastian/bastias/VowelsConsonants.java
+++ BastianBastias_COMP254Lab3/src/ca/centennial/comp254/lab3/exercise3/bastian/bastias/VowelsConsonants.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 public class VowelsConsonants {
 
 	private static final List<Character> VOWELS1 = Arrays.asList('a', 'e', 'i', 'o', 'u');
+	private static int vowelsCount=0;
 
 	/***
 	 * Write a recursive method that takes a string as argument and determines if
@@ -17,10 +18,14 @@ public class VowelsConsonants {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("Enter a string to check if it has more vowels than consonants: ");
-		String string = scanner.nextLine();
-		System.out.println(hasMoreVowels(string));
+		/*
+		 * Scanner scanner = new Scanner(System.in); System.out.
+		 * print("Enter a string to check if it has more vowels than consonants: ");
+		 * String string = scanner.nextLine();
+		 * System.out.println(hasMoreVowels(string));
+		 */
+
+		System.out.println(hasMoreVowels2("aaabaaaalllll"));
 	}
 
 	private static boolean hasMoreVowels(String word) {
@@ -31,15 +36,22 @@ public class VowelsConsonants {
 		return vowels > consonants;
 	}
 
-	
 	private static int vowelsCounter1(String word) {
 		final int wordLenght = word == null ? 0 : word.length();
 		if (wordLenght == 0) {
 			return 0;
 		}
-		// final char firstLetter = word.charAt(0);
-		// System.out.println((VOWELS.contains(word.substring(0,1))));
 		return (VOWELS1.contains(word.charAt(0)) ? 1 : 0) + vowelsCounter1(word.substring(1, wordLenght));
+	}
+
+	private static boolean hasMoreVowels2(String word) {
+		final int wordLenght = word == null ? 0 : word.length();
+		if (wordLenght == 0) {
+			return false;
+		}
+		vowelsCount += (VOWELS1.contains(word.charAt(0)) ? 1 : 0);
+		hasMoreVowels2(word.substring(1, wordLenght));
+		return (word.length() - vowelsCount) < vowelsCount;
 	}
 
 }
