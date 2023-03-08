117253455556693ff96dda5a7f793c572da55e21
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2023-03-08 14:16:46 -0500

    added lab4 exercise 3

--- BastianBastias_COMP254Lab4/src/ca/centennial/comp254/bbastias/lab4/Test.java
+++ BastianBastias_COMP254Lab4/src/ca/centennial/comp254/bbastias/lab4/Test.java
@@ -3,8 +3,9 @@ package ca.centennial.comp254.bbastias.lab4;
 public class Test {
 
 	public static void main(String[] args) {
-		exercise1();
-		exercise2();
+//		exercise1();
+//		exercise2();
+		exercise3();
 	}
 
 	private static void exercise1() {
@@ -37,6 +38,24 @@ public class Test {
 		System.out.println("final singlyLinkedStack: " + singlyLinkedStack);
 		System.out.println("final singlyLinkedStack2: " + singlyLinkedStack2);
 
+	}
+
+	private static void exercise3() {
+		LinkedQueue<Integer> linkedQ1 = new LinkedQueue<>();
+		linkedQ1.enqueue(10);
+		linkedQ1.enqueue(20);
+		linkedQ1.enqueue(30);
+		linkedQ1.enqueue(40);
+		LinkedQueue<Integer> linkedQ2 = new LinkedQueue<>();
+		linkedQ2.enqueue(50);
+		linkedQ2.enqueue(60);
+		linkedQ2.enqueue(70);
+		linkedQ2.enqueue(80);
+		System.out.println("original linkedQ1: " + linkedQ1);
+		System.out.println("original linkedQ2: " + linkedQ2);
+		linkedQ1.concatenate(linkedQ2);
+		System.out.println("final linkedQ1: " + linkedQ1);
+		System.out.println("final linkedQ2: " + linkedQ2);
 	}
 
 	private static <E> void transfer(Stack<E> S, Stack<E> T) {
