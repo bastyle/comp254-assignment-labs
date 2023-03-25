e5bff386eafa953b70df9a25335c6eb615b96d5a
Bastian Bastias Sanchez <bastian.bastias@gmail.com>
2023-03-25 16:56:49 -0400

    added exercise3

--- BastianBastias_COMP254Lab5/src/ca/centennial/comp254/bbastias/lab5/Test.java
+++ BastianBastias_COMP254Lab5/src/ca/centennial/comp254/bbastias/lab5/Test.java
@@ -1,9 +1,8 @@
 package ca.centennial.comp254.bbastias.lab5;
 
-import ca.centennial.comp254.bbastias.lab5.tree.AbstractTree;
+import ca.centennial.comp254.bbastias.lab5.tree.HeapPriorityQueue;
 import ca.centennial.comp254.bbastias.lab5.tree.LinkedBinaryTree;
 import ca.centennial.comp254.bbastias.lab5.tree.Position;
-import ca.centennial.comp254.bbastias.lab5.tree.Tree;
 
 public class Test {
 
@@ -31,33 +30,53 @@ public class Test {
 		exercise1B(lbt, eRight);
 		exercise1B(lbt, bLeft);
 		exercise1B(lbt, gRight);
+
+		exercise2(lbt, bLeft);
 		
-//		exercise2(lbt, bLeft);
+		exercise3();
 	}
 
 	private static <E> void exercise1(LinkedBinaryTree<String> T, Position<String> position) {
-		//System.out.println("----- exercise1A (preorderNext) ------");
+		// System.out.println("----- exercise1A (preorderNext) ------");
 //		Position<String> aux = LinkedBinaryTree.preorderNext(T, position);
-		//Position<String> aux = LinkedBinaryTree.orderNext(T, position, true);
-		//System.out.println("Next preorder position of " + position + " is " + aux);
+		// Position<String> aux = LinkedBinaryTree.orderNext(T, position, true);
+		// System.out.println("Next preorder position of " + position + " is " + aux);
 		Position<String> aux = T.preorder2(position);
 		System.out.println("Next preorder position of " + position + " is " + aux);
 	}
 
 	private static <E> void exercise1B(LinkedBinaryTree<String> T, Position<String> position) {
-		//System.out.println("----- exercise1B (inorderNext) ------");
+		// System.out.println("----- exercise1B (inorderNext) ------");
 //		Position<String> aux = LinkedBinaryTree.inorderNext(T, position);
 //		System.out.println("Next inorder position of " + position + " is " + aux);
 //		aux = T.inorder2(position);
 //		System.out.println("Next inorder position of " + position + " is " + aux);
 //		System.out.println("----------------------------");
-		Position<String> aux  = T.inorder3(position);
+		Position<String> aux = T.inorder3(position);
 		System.out.println("Next inorder position of " + position + " is " + aux);
-		
+
 	}
-	
+
 	private static void exercise2(LinkedBinaryTree<String> T, Position<String> position) {
-		System.out.println("----- exercise1B (inorderNext) ------");
+		System.out.println("----- exercise2 (postorderPrintHeight) ------");
 		T.postorderPrintHeight();
+	}
+
+	private static void exercise3() {
+		System.out.println("----- exercise13 (uphead recursive) ------");
+		HeapPriorityQueue<String, String> heapQueue = new HeapPriorityQueue<String, String>();
+		heapQueue.insert("47", "A");
+		heapQueue.insert("75", "C");
+		heapQueue.insert("28", "B");
+		heapQueue.insert("51", "D");
+		heapQueue.insert("31", "F");
+		heapQueue.insert("22", "G");
+		heapQueue.insert("15", "H");
+		int size = heapQueue.size();
+		for (int i = 0; i < size - 1; i++) {
+			System.out.println(
+					"(" + heapQueue.getHeap().get(i).getKey() + ", " + heapQueue.getHeap().get(i).getValue() + ")");
+			// System.out.println(heapQueue.removeMin().getKey());
+		}
 	}
 }
